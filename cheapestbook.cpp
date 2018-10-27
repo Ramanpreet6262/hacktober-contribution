533dbc260b56c58842f4bf1fb95cb7f0f10b1ea6
Ramanpreet Singh <ramanpreet6262@gmail.com>
2018-10-27 19:51:10 +0530

    added code

---
+++ cheapestbook.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+class book
+{
+	int price;
+	char title[50];
+	
+	public:
+		void getdata()
+		{
+			cin>>price>>title;
+		}
+		friend int cheapest(book a[], int n);
+		
+		
+};
+
+int main()
+{
+	int i,n,cprice;
+	cin>>n;
+	book a[n];
+	for(i=0;i<n;i++)
+	{
+		a[i].getdata();
+	}
+	cprice=cheapest(a,n);
+	
+	return 0;
+	
+}
+
+
+int cheapest(book a[], int n)
+{
+	int l=a[0].price;
+	int i;
+	for(i=0;i<n;i++)
+		{
+			if(a[i].price<l)
+			{
+				l=a[i].price;
+			}
+		}
+		return l;
+}
